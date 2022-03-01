6a4f25d5205a8f774ccd9cdb6a6acd1390224dc2
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2022-03-01 12:19:07 -0300

    fix: firefox binary

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
+options = webdriver.FirefoxOptions()
+options.binary_location = "/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox-bin"
+browser = webdriver.Firefox(options=options)
 browser.get("http://localhost:8000")
 
 assert 'Django' in browser.title
