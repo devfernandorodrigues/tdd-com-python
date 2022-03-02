88c7f9cb5a73b70740a863ed541c1d422110d327
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2022-03-02 16:05:50 -0300

    test: multiple users

--- app/functional_tests/tests.py
+++ app/functional_tests/tests.py
@@ -11,7 +11,7 @@ MAX_WAIT = 10
 
 class NewVisitorTestCase(LiveServerTestCase):
 
-    def setUp(self):
+    def new_browser(self):
         self.options = webdriver.FirefoxOptions()
         self.binary_location = (
             "/Applications/Firefox Developer "
@@ -19,6 +19,9 @@ class NewVisitorTestCase(LiveServerTestCase):
         )
         self.options.binary_location = self.binary_location
         self.browser = webdriver.Firefox(options=self.options)
+
+    def setUp(self):
+        self.new_browser()
 
     def tearDown(self):
         self.browser.quit()
@@ -41,7 +44,7 @@ class NewVisitorTestCase(LiveServerTestCase):
         rows = table.find_elements(by=By.TAG_NAME, value='tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Edith ouviu falar de uma nova aplicação online interessante para
         # lista de tarefas. Ela decide verificar sua homepage
         self.browser.get(self.live_server_url)
@@ -83,8 +86,51 @@ class NewVisitorTestCase(LiveServerTestCase):
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota
         # que o site gerou um URL único para ela -- há um pequeno
         # texto explicativo para isso
-        self.fail('Finish the test!')
 
         # Ela acessa esse URL - sua lista de tarefas continua lá.
 
         # Satisfeita, ela volta a dormir
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith inicia uma nova lista de tarefas
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Ela percebe que sua lista tem um URL único
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Agora um novo usuário, Francis, chega ao site.
+
+        ## Usamos uma nova sessão de navegador para garantir que nenhuma informação
+        ## de Edith está vindo de cookies etc
+        self.browser.quit()
+        self.new_browser()
+
+        # Francis accessa a página inicial. Não há nenhum sinal da lista de Edith
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis inicia uma nova lista inserindo um item novo. Ele
+        # é menos interessante que Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis obtém seu próprio URL exclusivo
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Novamente, não há nenhum sinal da lista de Edith
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfeitos, ambos voltam a dormir
