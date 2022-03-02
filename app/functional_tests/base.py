9f1a1965a6839c7af8fcfbb90d0a0bcf0b522e68
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2022-03-02 19:02:30 -0300

    chore: add individual files for tests

---
+++ app/functional_tests/base.py
@@ -0,0 +1,43 @@
+import os
+import time
+from django.test import LiveServerTestCase
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
+
+
+class FunctionalTest(LiveServerTestCase):
+
+    def new_browser(self):
+        self.options = webdriver.FirefoxOptions()
+        self.binary_location = (
+            "/Applications/Firefox Developer "
+            "Edition.app/Contents/MacOS/firefox-bin"
+        )
+        self.options.binary_location = self.binary_location
+        self.browser = webdriver.Firefox(options=self.options)
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def setUp(self):
+        self.new_browser()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements(by=By.TAG_NAME, value='tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
--- app/functional_tests/test_simple_list_creation.py
+++ app/functional_tests/test_simple_list_creation.py
@@ -1,53 +1,7 @@
-import os
-import time
-from django.test import LiveServerTestCase
-
-from selenium import webdriver
+from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
-
-
-class NewVisitorTestCase(LiveServerTestCase):
-
-    def new_browser(self):
-        self.options = webdriver.FirefoxOptions()
-        self.binary_location = (
-            "/Applications/Firefox Developer "
-            "Edition.app/Contents/MacOS/firefox-bin"
-        )
-        self.options.binary_location = self.binary_location
-        self.browser = webdriver.Firefox(options=self.options)
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-
-    def setUp(self):
-        self.new_browser()
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements(by=By.TAG_NAME, value='tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements(by=By.TAG_NAME, value='tr')
-        self.assertIn(row_text, [row.text for row in rows])
-
+class NewVistorTest(FunctionalTest):
     def test_can_start_a_list_for_one_user(self):
         # Edith ouviu falar de uma nova aplicação online interessante para
         # lista de tarefas. Ela decide verificar sua homepage
@@ -81,7 +35,6 @@ class NewVisitorTestCase(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use a peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em sua lisat
         self.wait_for_row_in_list_table('2: Use a peacock feathers to make a fly')
@@ -139,27 +92,3 @@ class NewVisitorTestCase(LiveServerTestCase):
 
         # Satisfeitos, ambos voltam a dormir
 
-    def test_layout_and_styling(self):
-        # Edith acessa a página inicial
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # Ela perecebe que a caixa de entrada está elegantemente centralizada
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
-
-        # Ela inicia uma nova lista e vê que a entrada está elegantemente
-        # centralizada aí também
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
