132d3d6a048ad4c84210eee04be46abc952df669
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2022-03-03 15:51:38 -0300

    fix: create session on server

--- app/functional_tests/server_tools.py
+++ app/functional_tests/server_tools.py
@@ -13,4 +13,4 @@ def create_session_on_server(email):
     heroku_conn = heroku3.from_key(HEROKU_API_KEY)
     app = heroku_conn.apps()[HEROKU_STAGING_APP_NAME]
     session_key = app.run_command(f'python app/manage.py create_session {email}')
-    return session_key.split()[-1]
+    return session_key[0].split()[-1]
