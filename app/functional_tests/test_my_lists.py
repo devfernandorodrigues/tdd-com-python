f5fbb3360e1f4f390ba3cdd316086a0ff13c15bf
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2022-03-03 16:24:47 -0300

    feat: url, placeholder view, and first-cut templates for my lists

--- app/functional_tests/test_my_lists.py
+++ app/functional_tests/test_my_lists.py
@@ -36,3 +36,44 @@ class MyListTest(FunctionalTest):
         self.create_pre_authenticated_session(email)
         self.browser.get(self.live_server_url)
         self.wait_to_be_logged_in(email)
+
+        # Edith acessa a página inicial e começa uma lista
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Reticulate splines')
+        self.add_list_item('Immanentize eschaton')
+        first_list_url = self.browser.current_url
+
+        # Ela precebe o link para "My lists" pela primeira vez.
+        self.browser.find_element_by_link_text('My lists').click()
+
+        # Ela vê que sua lista está lá, nomeada com
+        # o primeiro item da lista
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Reticulate splines')
+        )
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
+
+        # Ela decide iniciar outra lista, somente para conferir
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Click cows')
+        second_list_url = self.browser.current_url
+
+        # Em "my lists", sua nova lista aparece
+        self.browser.find_element_by_link_text('My lists').click()
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Click cows')
+        )
+        self.browser.find_element_by_link_text('Click cows').click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, second_list_url)
+        )
+
+        # Ela faz logout A opção "My lists" desaparece
+        self.browser.find_element_by_link_text('Log out').click()
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_link_text('My lists'),
+            []
+        ))
